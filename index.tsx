be1011a40c68856387bfc91c56a1bdbec778dea8
eat1st-404 <cathwhite404@gmail.com>
2026-01-08 14:55:54 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,23 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// 1. 引入 PostHog 库
+import posthog from 'posthog-js';
+
+// 2. 初始化 (我已经帮你填好了截图里的 Key 和 Host)
+// 这样你不需要去 Cloudflare 设置任何环境变量，上传就能用
+posthog.init('phc_tMPOzuk75LgqKYRVtGsLfXuXOE6ropIrxOpBXLaoB3R', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only', 
+});
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
